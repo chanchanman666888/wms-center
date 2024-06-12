7b856fb176f0656a20c5ace08ab55bad827e5f55
chanchanman666888 <zhang.lei@shipsaving.com>
2024-06-12 14:04:49 +0800

    first commit

---
+++ src/main/java/com/shipsaving/wms/wmscenter/WmsCenterApplication.java
@@ -0,0 +1,13 @@
+package com.shipsaving.wms.wmscenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WmsCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WmsCenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shipsaving/wms/wmscenter/WmsCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shipsaving.wms.wmscenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WmsCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
